67bc9a151287dd95fe3d97fa9c74ed4dd01c49f0
emilykpritchard <emilykpritchard@me.com>
2023-01-11 15:56:04 +0000

    Nearly there, fetch not working when logged in

--- cosmic-calc/src/Context/useUser.js
+++ cosmic-calc/src/Context/useUser.js
@@ -4,36 +4,35 @@ import { auth } from "../pages/firebaseConfig";
 // import NavBar from "../components/navBar/NavBar";
 import React from "react";
 
-export const UserContext = createContext();
+export const UserContext = createContext({});
 
 export function UserContextProvider(props) {
   const [userData, setUserData] = useState({});
 
   useEffect(() => {
+    const retrieveUserData = async (user) => {
+      let email = user.email;
+      const response = await fetch(
+        `http://localhost:4000/api/users/email/${email}`
+      );
+      const data = await response.json();
+
+      setUserData(data.payload);
+
+      return data.payload;
+    };
     onAuthStateChanged(auth, (user) => {
-      retrieveUserData(user);
+      if (user) {
+        retrieveUserData(user);
+      }
     });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const retrieveUserData = async (user) => {
-    let email = user.email;
-    const response = await fetch(
-      `http://localhost:4000/api/users/email/${email}`
-    );
-    const data = await response.json();
-
-    setUserData(data.payload);
-
-    return data.payload;
-  };
-
+  console.log(userData);
   return (
-    <div>
-      <UserContextProvider.Provider value={userData}>
-        {props.children}
-      </UserContextProvider.Provider>
-    </div>
+    <UserContext.Provider value={userData}>
+      {props.children}
+    </UserContext.Provider>
   );
 }
 
