6cd9f7583977120d3ec1801ed8ec16941d668e21
LouisWright <louis--wright@hotmail.co.uk>
2023-01-16 15:36:27 +0000

    added conditions for not going into negative numbers for states/body part ids

--- cosmic-calc/src/pages/Avatars.js
+++ cosmic-calc/src/pages/Avatars.js
@@ -2,10 +2,10 @@ import React, { useState, useContext } from "react";
 import "./Avatars.css";
 import { ScoreContext } from "../components/score/ScoreContext";
 
-let headParts = [1, 2, 3];
-let bodyParts = [1, 2, 3, 4];
-let antParts = [1, 2, 3, 4];
-let teethParts = [1, 2, 3, 4, 5];
+// let headParts = [1, 2, 3];
+// let bodyParts = [1, 2, 3, 4];
+// let antParts = [1, 2, 3, 4];
+// let teethParts = [1, 2, 3, 4, 5];
 
 const AvatarBuilder = () => {
   // initialize state for the selected head, body, and legs options
@@ -14,84 +14,114 @@ const AvatarBuilder = () => {
   const [selectedAnt, setSelectedAnt] = useState(1);
   const [selectedTeeth, setSelectedTeeth] = useState(1);
 
+  // body part amounts
+  let bodyPartAmounts = {
+    Heads: 3,
+    Bodies: 4,
+    Ants: 4,
+    Teeth: 1,
+  };
+
   // import the context
   let context = useContext(ScoreContext);
 
   function handleHeadClick(bodypart, direction) {
-    if (bodypart === "head" && direction === "left") {
+    if (bodypart === "head" && direction === "left" && selectedHead !== 1) {
       setSelectedHead(selectedHead - 1);
-    } else if (bodypart === "head" && direction === "right") {
+    } else if (
+      bodypart === "head" &&
+      direction === "right" &&
+      selectedHead !== bodyPartAmounts.Heads
+    ) {
       setSelectedHead(selectedHead + 1);
       console.log(selectedHead);
     }
 
-    if (bodypart === "body" && direction === "left") {
+    if (bodypart === "body" && direction === "left" && selectedBody !== 1) {
       setSelectedBody(selectedBody - 1);
-    } else if (bodypart === "body" && direction === "right") {
+    } else if (
+      bodypart === "body" &&
+      direction === "right" &&
+      selectedBody !== bodyPartAmounts.Bodies
+    ) {
       setSelectedBody(selectedBody + 1);
     }
 
-    if (bodypart === "ant" && direction === "left") {
+    if (bodypart === "ant" && direction === "left" && selectedAnt !== 1) {
       setSelectedAnt(selectedAnt - 1);
-    } else if (bodypart === "ant" && direction === "right") {
+    } else if (
+      bodypart === "ant" &&
+      direction === "right" &&
+      selectedBody !== bodyPartAmounts.Ants
+    ) {
       setSelectedAnt(selectedAnt + 1);
     }
 
-    if (bodypart === "teeth" && direction === "left") {
+    if (bodypart === "teeth" && direction === "left" && selectedTeeth !== 1) {
       setSelectedTeeth(selectedTeeth - 1);
-    } else if (bodypart === "teeth" && direction === "right") {
+    } else if (
+      bodypart === "teeth" &&
+      direction === "right" &&
+      selectedBody !== bodyPartAmounts.Teeth
+    ) {
       setSelectedTeeth(selectedTeeth + 1);
     }
     console.log("This is user Context", context.user.email);
   }
 
-  // async function handleSubmit() {
-  //   let email = await user.email;
-
-  //   const response = await fetch(
-  //     `http://localhost:3001/api/users/email/${email}`,
-  //     {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ total_score: score }),
-  //     }
-  //   );
-  //   const data = await response.json();
-  //   console.log(data);
-  // }
-
-  async function getAvatars (email){
-    const response = await fetch(`http://localhost:3001/api/users/avatars/${email}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  async function getAvatars(email) {
+    const response = await fetch(
+      `http://localhost:3001/api/users/avatars/${email}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     const data = await response.json();
-    console.log(data);
+    console.log("data", data);
+    setSelectedAnt(data.payload.antid);
+    setSelectedBody(data.payload.bodyid);
+    setSelectedHead(data.payload.headid);
   }
 
-  async function patchAvatars (email, bodyNum, antNum, headNum) {
-    console.log("hello")
-    const response = await fetch(`http://localhost:3001/api/users/avatars/${email}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ bodyId: bodyNum , antId: antNum, headId: headNum }),
-    });
+  async function patchAvatars(email, bodyNum, antNum, headNum) {
+    console.log("hello");
+    const response = await fetch(
+      `http://localhost:3001/api/users/avatars/${email}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          bodyId: bodyNum,
+          antId: antNum,
+          headId: headNum,
+        }),
+      }
+    );
     const data = await response.json();
     console.log(data);
   }
 
   return (
     <div>
-    <br/>
-    <br/>
-
-      <button onClick={() => patchAvatars(context.user.email, selectedBody, selectedAnt, selectedHead)}>Submit</button>
+      <br />
+      <br />
+      <button
+        onClick={() =>
+          patchAvatars(
+            context.user.email,
+            selectedBody,
+            selectedAnt,
+            selectedHead
+          )
+        }
+      >
+        Submit
+      </button>
       <div className="head-selector">
         <button onClick={() => handleHeadClick("head", "left")}>⬅</button>
         Head
@@ -111,6 +141,7 @@ const AvatarBuilder = () => {
         <button onClick={() => handleHeadClick("ant", "left")}>⬅</button>
         Antenna
         <button onClick={() => handleHeadClick("ant", "right")}>➡</button>
+        <button onClick={() => getAvatars(context.user.email)}>TEST</button>
       </div>
 
       <div class="container">
@@ -135,7 +166,6 @@ const AvatarBuilder = () => {
 
                 <div class="nose"></div>
                 <div class="mouth">
-                  <div class="mouth"></div>
                   <div class="tooth_1"></div>
                   <div class="tooth_2"></div>
                   <div class="tooth_3"></div>
