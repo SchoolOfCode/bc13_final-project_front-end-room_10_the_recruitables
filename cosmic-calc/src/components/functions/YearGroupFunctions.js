21815b88d2bf3182ee1a84d316c51e366a6e0087
jason_riley <jason.rileycode@hotmail.com>
2023-01-17 13:55:24 +0000

    Halfway through Year 2 functions

--- cosmic-calc/src/components/functions/YearGroupFunctions.js
+++ cosmic-calc/src/components/functions/YearGroupFunctions.js
@@ -1,24 +1,28 @@
-// Reusable function to generate a random number from 0 to input
+// Reusable function to generate a random number from 0 to input - 1
 function randomNumberGenerator(num) {
     return Math.floor(Math.random() * num);
 }
 
-// Year 1 Planet 4 - "Number bonds to 10 and 20"
-export function yearOnePlanetFour() {
+
+// Year 1 Planet 4 / Planet 8 - "Number bonds to 10" / "Number bonds to 20"
+export function yearOnePlanetFourQuestion(planet) {
     // Randomly choose if doing number bonds to 10 or 20
-    let total;
-    if (Math.random() < 0.5) {
-        total = 10;
-    } else {
+    let total = 10;
+    if (planet === 8) {
         total = 20;
     }
     // Randomly select a number which the user must find the pair of
     let knownValue = randomNumberGenerator(total + 1);
     return [knownValue, total];
 }
+export function yearOnePlanetFourAnswer(values, playerAnswer) {
+    let correctAnswer = values[1] - values[0];
+    return [parseInt(playerAnswer) === correctAnswer, correctAnswer];
+}
 
-// Year 1 Planet 5 / Planet 8 - "Adding and subtracting numbers", easier and harder
-export function yearOnePlanetFive(planet) {
+
+// Year 1 Planet 5 - "Adding and subtracting numbers"
+export function yearOnePlanetFiveQuestion() {
     // Randomly choose if adding or subtracting
     let operation;
     if (Math.random() < 0.5) {
@@ -29,13 +33,7 @@ export function yearOnePlanetFive(planet) {
 
     let values = [];
     for (let i = 0; i < 2; i++) {
-        let randomInteger;
-        // Choose the range of random values depending on whether Planet 5 or 8
-        if (planet === 5) {
-            randomInteger = Math.floor(10) + 1;
-        } else {
-            randomInteger = Math.floor(50) + 1;
-        }
+        let randomInteger = randomNumberGenerator(10) + 1;
         values.push(randomInteger);
     }
 
@@ -46,9 +44,19 @@ export function yearOnePlanetFive(planet) {
     }
     return [values[0], operation, values[1]];
 }
+export function yearOnePlanetFiveAnswer(values, playerAnswer) {
+    let correctAnswer;
+    if (values[1] === "+") {
+        correctAnswer = values[0] + values[2];
+    } else {
+        correctAnswer = values[0] - values[2];
+    }
+    return [parseInt(playerAnswer) === correctAnswer, correctAnswer];
+}
+
 
 // Year 1 Planet 6 - "Read a number as a word and convert it into a number"
-export function yearOnePlanetSix() {
+export function yearOnePlanetSixQuestion() {
     const data = [
         "zero",
         "one",
@@ -75,10 +83,15 @@ export function yearOnePlanetSix() {
     let index = randomNumberGenerator(21);
     return [index, data[index]]; // Returns the number in both numerical and word form
 }
+export function yearOnePlanetSixAnswer(values, playerAnswer) {
+    let correctAnswer = values[0]
+    return [parseInt(playerAnswer) === correctAnswer, correctAnswer];
+}
+
 
 // Year 2 Planet 2 - "Add or subtract in steps of 10 from any number"
 // REWORK SO THAT EDGE CASES (i.e.: 85) DO NOT HAVE A VERY HIGH CHANCE (50%) OF GIVING THE SAME QUESTION (Add 10 to 85 one time)
-export function yearTwoPlanetTwo() {
+export function yearTwoPlanetTwoQuestion() {
     let initialValue = randomNumberGenerator(101);
     if (initialValue > 90) {
         // If the initial value is too large to increase, as it will go over 100
@@ -105,11 +118,58 @@ export function yearTwoPlanetTwo() {
         return [initialValue, operation, steps];
     }
 }
+export function yearTwoPlanetTwoAnswer(values, playerAnswer) {
+    if (values[1] === "+") {
+        let correctAnswer = values[0] + 10 * values[2];
+        return [parseInt(playerAnswer) === correctAnswer, correctAnswer];
+    } else {
+        let correctAnswer = values[0] - 10 * values[0];
+        return [parseInt(playerAnswer) === correctAnswer, correctAnswer];
+    }
+}
 
-// Year 2 Planet 4 - "Place value (e.g.: How many 10s in 72?)"
-export function yearTwoPlanetFour() {
+
+// Year 2 Planet 5 - "Add together three single-digit numbers"
+export function yearTwoPlanetFiveQuestion() {
+    let values = [];
+    values.push(randomNumberGenerator(9) + 1);
+    values.push(10 - values[0]);
+    values.push(randomNumberGenerator(9) + 1);
+
+    // Randomly rearrange the values
+    let rearrangedValues = [];
+    for (let i = 0; i < 3; i++) {
+        let maxIndex = values.length;
+        let randomIndex = randomNumberGenerator(maxIndex);
+        rearrangedValues.push(values[randomIndex]);
+        values.splice(randomIndex, 1);
+    }
+    return rearrangedValues;
+}
+export function yearTwoPlanetFiveAnswer(values, playerAnswer) {
+    let correctAnswer = values[0] + values[1] + values[2];
+    return [parseInt(playerAnswer) === correctAnswer, correctAnswer];
+}
+
+
+// Year 2 Planet 6 - "Fractions of numbers"
+export function yearTwoPlanetSixQuestion() {
+    let denominator = randomNumberGenerator(3) + 2;
+    let numerator = randomNumberGenerator(denominator) + 1;
+    let otherValue = (randomNumberGenerator(10) + 1) * denominator;
+    return [numerator, denominator, otherValue];
+}
+export function yearTwoPlanetSixAnswer(values, playerAnswer) {
+    let correctAnswer = values[2] * values[0] / values[1];
+    return [parseInt(playerAnswer) === correctAnswer, correctAnswer];
+}
+
+
+// Year 2 Planet 7 - "Sort a set of numbers into order"
+export function yearTwoPlanetSevenQuestion() {
 
 }
+
 
 function valueChooser(chances) {
     const valuesEasy = [0, 1, 2, 3, 5, 10];
