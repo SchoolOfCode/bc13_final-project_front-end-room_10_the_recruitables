4f6ef9da3eb5ee18e2a5fe8be46004f15377cfa8
L-McHugh <l.mchugh1@outlook.com>
2023-01-18 16:41:27 +0000

    shape game

--- cosmic-calc/src/pages/YearTwoGames.js
+++ cosmic-calc/src/pages/YearTwoGames.js
@@ -4,31 +4,47 @@ import onePenny from "../images/coins/1p.png";
 import "./yearTwoGames.css";
 
 export default function YearTwoGames() {
-  const [tenPence, setTenPence] = useState(Math.floor(Math.random() * 10) + 1);
-  const [onePence, setOnePence] = useState(Math.floor(Math.random() * 10) + 1);
-  const [answer, setAnswer] = useState(0);
+  // const [tenPence, setTenPence] = useState(Math.floor(Math.random() * 10) + 1);
+  // const [onePence, setOnePence] = useState(Math.floor(Math.random() * 10) + 1);
+  // const [answer, setAnswer] = useState(0);
 
-  function giveRandomCoin() {
-    setTenPence(Math.floor(Math.random() * 10) + 1);
-    setOnePence(Math.floor(Math.random() * 10) + 1);
+  // function giveRandomCoin() {
+  //   setTenPence(Math.floor(Math.random() * 10) + 1);
+  //   setOnePence(Math.floor(Math.random() * 10) + 1);
+  // }
+
+  // let tenPenceTotal = tenPence * 10;
+  // let onePenceTotal = onePence * 1;
+  // let total = tenPenceTotal + onePenceTotal;
+
+  // function checkAnswer() {
+  //   console.log("answer", answer);
+  //   if (String(answer) === String(total)) {
+  //     console.log("correct");
+  //   } else {
+  //     console.log("wrong");
+  //   }
+  // }
+
+const [shape, setShape] = useState("square")
+
+const shapes = ["square", "rectangle", "circle", "triangle", "pentagon", "hexagon", "heptagon", "octagon"]
+
+function giveRandomShape() {
+  setShape(shapes[Math.floor(Math.random() * shapes.length)])
+}
+
+function checkAnswer(answer) {
+  if (answer === shape) {
+    console.log("correct")
+  } else {
+    console.log("wrong")
   }
-
-  let tenPenceTotal = tenPence * 10;
-  let onePenceTotal = onePence * 1;
-  let total = tenPenceTotal + onePenceTotal;
-
-  function checkAnswer() {
-    console.log("answer", answer);
-    if (String(answer) === String(total)) {
-      console.log("correct");
-    } else {
-      console.log("wrong");
-    }
-  }
+}
 
   return (
     <div className="bigdaddy">
-      <button onClick={giveRandomCoin}>Give me a coin</button>
+      {/* <button onClick={giveRandomCoin}>Give me a coin</button>
       <div className="images-div">
         {Array.from({ length: tenPence }, (_, i) => (
           <img key={i} src={tenPenny} alt="random" className="ten-pence-pic" />
@@ -46,7 +62,18 @@ export default function YearTwoGames() {
             setAnswer("");
           }
         }}
-      />
+      /> */}
+      <h1>Click on the {shape}?</h1> 
+      <button onClick={giveRandomShape}>Give me a shape</button>
+      <button onClick={() => checkAnswer("square")}>square</button>
+      <button onClick={() => checkAnswer("rectangle")}>rectangle</button>
+      <button onClick={() => checkAnswer("circle")}>circle</button>
+      <button onClick={() => checkAnswer("triangle")}>triangle</button>
+      <button onClick={() => checkAnswer("pentagon")}>pentagon</button>
+      <button onClick={() => checkAnswer("hexagon")}>hexagon</button>
+      <button onClick={() => checkAnswer("heptagon")}>heptagon</button>
+      <button onClick={() => checkAnswer("octagon")}>octagon</button>
+    
     </div>
   );
 }
