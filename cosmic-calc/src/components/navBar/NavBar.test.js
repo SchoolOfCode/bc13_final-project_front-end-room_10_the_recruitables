707314605d0c381edd15d4e9b845044b875a97ab
JeremyXZ <jeremyuk168@gmail.com>
2023-01-23 17:08:34 +0000

    NavBar test done

--- cosmic-calc/src/components/navBar/NavBar.test.js
+++ cosmic-calc/src/components/navBar/NavBar.test.js
@@ -1,15 +1,47 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { screen, render } from "@testing-library/react";
+import {screen, render } from "@testing-library/react";
+import {BrowserRouter, Router} from 'react-router-dom';
 import userEvent from "@testing-library/user-event";
 
 import NavBar from "./NavBar";
 
 describe("<NavBar/>", () => {
-  const mockedProps = {
-    ButtonNumber: 7,
-    clickToGame: jest.fn(),
-    text: "Go to level 7"
-  };
+    it("successfully renders the component, matches snapshot", () => {
+        const { asFragment } = render(
+            <BrowserRouter>
+                <NavBar />
+            </BrowserRouter>
+        
+        );
+        expect(asFragment()).toMatchSnapshot();
+      });
+
+    it('there are 4 links on the screen', () => {
+        render(
+            <BrowserRouter>
+                <NavBar />
+            </BrowserRouter>
+        
+        );
+        screen.debug()
+        const links = screen.getAllByRole('link')
+        expect(links.length).toBe(4)
+    })
+
+    it('renders all expected link texts to the screen', () => {
+        render(
+            <BrowserRouter>
+                <NavBar />
+            </BrowserRouter>
+        
+        );
+        const linkTexts = ['Profile', 'Progress', 'Game']
+
+        screen.debug()
+        const linkTexts = screen.getAllByText()
+        const matchedItems= linkTexts.every(item => [...links].includes(item))
+       console.log(matchedItems)
+    })
 
 })
