2df38e0efe8371e7b58efabdd61eac9263249202
jason_riley <jason.rileycode@hotmail.com>
2023-01-23 15:48:01 +0000

    Y2P2 test debugged

--- cosmic-calc/src/components/functions/Year2Functions.test.js
+++ cosmic-calc/src/components/functions/Year2Functions.test.js
@@ -7,20 +7,24 @@ describe("Y2P2 - add or subtract in step of 10 from 100.", () => {
     test("Ensure we get a result between 0 and 100 in .", async () => {
         expect(questionValue[0]).toBeGreaterThanOrEqual(0);
         expect(questionValue[0]).toBeLessThanOrEqual(100);
+    });
+    test("Ensure we get an operator which is either addition or subtraction.", async () => {
         expect(questionValue[1] === "+" || questionValue[1] === "-").toBeTruthy();
+    });
+    test("Ensure the number we are adding or subtracting is a multiple of 10.", async () => {
         expect(questionValue[2] % 10).toBe(0);
     });
     for (let i = 0; i < 10; i++) {
-        questionValue = yearTwoPlanetTwoQuestion()
-        test(`Test ${i} of 10. Question is ${questionValue[0]} ${questionValue[1]} ${questionValue[2]}`, async () => {
+        let questionValue = yearTwoPlanetTwoQuestion();
+        test(`Test ${i} of 10. Question is ${questionValue[0]} ${questionValue[1]} ${questionValue[2]}.`, async () => {
             let playerAnswer;
-            if(questionValue[1] === "+") {
+            if (questionValue[1] === "+") {
                 playerAnswer = questionValue[0] + questionValue[2]
             } else {
                 playerAnswer = questionValue[0] - questionValue[2]
             }
             let actual = yearTwoPlanetTwoAnswer(questionValue, playerAnswer);
-            expect(actual).toStrictEqual([true, questionValue]);
+            expect(actual).toStrictEqual([true, playerAnswer]);
         });
     }
 });
