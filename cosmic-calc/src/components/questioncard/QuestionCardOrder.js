90d3dc5bc92563c042a92d8d3aeb2ab7b79ae0af
emilykpritchard <emilykpritchard@me.com>
2023-01-24 16:02:33 +0000

    Years 3 and 4

--- cosmic-calc/src/components/questioncard/QuestionCardOrder.js
+++ cosmic-calc/src/components/questioncard/QuestionCardOrder.js
@@ -1,27 +1,29 @@
 import React from "react";
-import "./questionCard.css";
+import "./questionCardOrder.css";
 
 export default function QuestionCardOrder(props) {
   return (
     <div className="questionDiv">
-    <h1> {props.h1}</h1>
-      <h2 className="h2QuestionGame">{props.noOfQuestions + ")  "}</h2>
-      <h2 className="h2QuestionGame">
-        {props.value1}, {props.value2}, {props.value3}, {props.value4}
-      </h2>
-      <input
-        className="inputGame"
-        type="text"
-        value={props.answerInput}
-        onChange={(e) => props.setAnswerInput(e.target.value)}
-        onKeyDown={(e) => {
-          // console.log(e);
-          if (e.key === "Enter") {
-            props.checkAnswer();
-            props.setAnswerInput("");
-          }
-        }}
-      />
+      <h1> {props.h1}</h1>
+      <div className="questionDisplay">
+        <h2 className="h2QuestionGame">{props.noOfQuestions + ")  "}</h2>
+        <h2 className="h2QuestionGame">
+          {props.value1}, {props.value2}, {props.value3}, {props.value4}
+        </h2>
+        <input
+          className="inputGame"
+          type="text"
+          value={props.answerInput}
+          onChange={(e) => props.setAnswerInput(e.target.value)}
+          onKeyDown={(e) => {
+            // console.log(e);
+            if (e.key === "Enter") {
+              props.checkAnswer();
+              props.setAnswerInput("");
+            }
+          }}
+        />
+      </div>
     </div>
   );
 }
